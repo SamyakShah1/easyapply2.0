3e90c1d231c0b58b1be20c0d308fea17f4df32d9
SamyakShah1 <samyakshah2004@gmail.com>
2026-08-01 12:35:54 +0530

    Delete old Desktop Auto-Apply architecture and UI

--- drivers/__init__.py
+++
@@ -1,7 +0,0 @@
-# Modular ATS Drivers Package
-from .greenhouse import apply_greenhouse
-from .smartrecruiters import apply_smartrecruiters
-from .zohorecruit import apply_zohorecruit
-from .lever import apply_lever
-from .workday import apply_workday
-from .generic import apply_generic
